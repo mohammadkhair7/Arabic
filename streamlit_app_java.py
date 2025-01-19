0cb9cdaf3348e877be74bcbcab76839ff862259c
Mohammad Mohammad Khair <mohammadkhair@gmail.com>
2025-01-19 14:35:52 -0600

    comment

--- streamlit_app_java.py
+++ streamlit_app_java.py
@@ -256,6 +256,7 @@ def main():
                 # Access Java classes
                 # st.write("Debug: Accessing Java class")
                 try:
+                    print("instantiating class AlKhalil2AnalyzerWrapper")
                     java_class = jpype.JClass("net.oujda_nlp_team.AlKhalil2AnalyzerWrapper")
                     java_object = java_class()
                     # st.write("Debug: Java class accessed successfully")
